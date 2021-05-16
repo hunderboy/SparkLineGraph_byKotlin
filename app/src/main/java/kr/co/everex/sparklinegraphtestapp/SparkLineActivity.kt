3516060f22f128b9e1a98994ab97cb24a3eb42a9
Dev_dominos <masury2011@gmail.com>
2021-05-17 05:42:24 +0900

    // 라인 커스텀

--- app/src/main/java/kr/co/everex/sparklinegraphtestapp/SparkLineActivity.kt
+++ app/src/main/java/kr/co/everex/sparklinegraphtestapp/SparkLineActivity.kt
@@ -14,13 +14,19 @@ class SparkLineActivity : AppCompatActivity() {
 //    val arrayData = arrayListOf(10,12,18,22,32,40)
 //    val arrayData = arrayListOf(10,12,18,22,32,40)
 
+    // 커스텀
+    val arrayDataCustom = arrayListOf(80,74,32,45,100,100,25)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySparkLineBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view) // 뷰 바인딩 적용 완료
 
+        // 라인에 데이터 설정
         binding.sparkline.setData(arrayData)
-
+        binding.sparklineCustom.setData(arrayDataCustom)
     }
+
+
 }
